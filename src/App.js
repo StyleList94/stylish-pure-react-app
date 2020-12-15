6ae17e452ae6a811c1c02ca0c9176ac58bba9b07
LovelyHaRa <codetender94@gmail.com>
2020-12-15 20:46:31 +0900

    initialize css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import "./App.css";
+
 const App = () => {
-  return <div>Hello React!</div>;
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <p>Stylish React App</p>
+      </header>
+    </div>
+  );
 };
 
 export default App;
