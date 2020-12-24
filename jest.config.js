74b849f358b5e607b05f39fd6a44c558bde1cb04
LovelyHaRa <codetender94@gmail.com>
2020-12-24 11:25:12 +0900

    mocking css modules

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   testPathIgnorePatterns: ["<rootDir>/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
 };
