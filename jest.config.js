6417e1a74b62f02a8334db04510357140c04173c
LovelyHaRa <codetender94@gmail.com>
2021-04-17 11:13:05 +0900

    feat: update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,11 @@ module.exports = {
   testPathIgnorePatterns: ["<rootDir>/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   transform: {
-    "^.+\\.js$": "babel-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
   moduleNameMapper: {
-    "\\.(css|less)$": "identity-obj-proxy",
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "identity-obj-proxy",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
 };
